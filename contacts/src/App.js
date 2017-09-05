c811ffde225569401c9aff6641bb8efc949210bc
Felipe Rodrigues <fr.oliveira94@gmail.com>
2017-09-05 07:12:21 -0300

    add: navigation

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -29,7 +29,10 @@ class App extends Component {
         {this.state.screen === 'list' && (
           <ListContacts 
             onDeleteContact={ this.removeContact } 
-            contacts={ this.state.contacts } />
+            contacts={ this.state.contacts }
+            onNavigate={() => {
+              this.setState({ screen: 'create' })
+            }} />
         )}
         {this.state.screen === 'create' && (
           <CreateContact />
