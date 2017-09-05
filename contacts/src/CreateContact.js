f12697c17e97f199df6d490b457893048096f252
Felipe Rodrigues <fr.oliveira94@gmail.com>
2017-09-05 09:10:04 -0300

    add: html from add contacts page

--- contacts/src/CreateContact.js
+++ contacts/src/CreateContact.js
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput'; 
 
 class CreateContact extends Component {
     render() {
         return(
-            <div>Create Contact!</div>
+            <div>
+                <Link className="close-create-contact" to="/">Close</Link>
+                <form className="create-contact-form">
+                    <ImageInput 
+                        className="create-contact-avatar-input"
+                        name="avatarURL"
+                        maxHeight={64}
+                    />
+                </form>
+                <div className="create-contact-details">
+                    <input name="name" type="text" placeholder="name" />
+                    <input name="email" type="text" placeholder="Email" />
+                    <button>Add Contact</button>
+                </div>
+            </div>
         )
     }
 }
